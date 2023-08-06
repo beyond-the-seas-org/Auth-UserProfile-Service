24fec7c1343d9d2736a25473661053c61a91f104
Md. Asif Haider <1805112@ugrad.cse.buet.ac.bd>
2023-08-06 17:18:47 +0000

    added cors

--- user/__init__.py
+++ user/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import psycopg2, dotenv, os
 from flask_restx import Api
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app,origins = 'http://localhost:8000')
+
 
 dotenv.load_dotenv()
 db_url = os.getenv('DATABASE_URL')
