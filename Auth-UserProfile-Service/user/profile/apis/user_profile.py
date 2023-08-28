be40d5d845d7d5736755c14a3a20aa71a8a54fe7
asifhaider <1805112@ugrad.cse.buet.ac.bd>
2023-08-28 11:48:27 +0600

    jwt auth bypass for profile page design

--- Auth-UserProfile-Service/user/profile/apis/user_profile.py
+++ Auth-UserProfile-Service/user/profile/apis/user_profile.py
@@ -16,7 +16,7 @@ authorization_header.add_argument('Authorization', type=str, location='headers',
 class UpdateUserProfiles(Resource):
     @profile.doc(responses={200: 'OK', 404: 'Not Found', 500: 'Internal Server Error'})
     @profile.expect(update_model, authorization_header)
-    @jwt_required()
+
     def put(self, user_id):
         # parse the request body
         request_body = request.get_json()
@@ -25,6 +25,7 @@ class UpdateUserProfiles(Resource):
         if student:
             # update the user profile
             student.username = request_body['username']
+            
             student.first_name = request_body['first_name']
             student.last_name = request_body['last_name']
             student.primary_email = request_body['primary_email']
@@ -32,16 +33,20 @@ class UpdateUserProfiles(Resource):
             student.age = request_body['age']
             student.password_hash = generate_password_hash(request_body['password_hash'])
             student.secondary_email = request_body['secondary_email']
+            
             student.bsc_year_of_passing = request_body['bsc_year_of_passing']
             student.ms_year_of_passing = request_body['ms_year_of_passing']
             student.bsc_cgpa = request_body['bsc_cgpa']
             student.ms_cgpa = request_body['ms_cgpa']
             student.bsc_university = request_body['bsc_university']
             student.ms_university = request_body['ms_university']
-            student.github_link = request_body['github_link']
+
+            student.github_link = request_body['github_link']            
             student.linkedin_link = request_body['linkedin_link']
             student.website_link = request_body['website_link']
+
             student.current_address = request_body['current_address']
+
             student.gre_verbal_quant_score = request_body['gre_verbal_quant_score']
             student.gre_awa_score = request_body['gre_awa_score']
             student.toefl_score = request_body['toefl_score']
@@ -58,7 +63,7 @@ class UpdateUserProfiles(Resource):
 @profile.route('/<int:user_id>')
 class GetUserProfiles(Resource):
     @profile.doc(responses={200: 'OK', 404: 'Not Found', 500: 'Internal Server Error'}, parser=authorization_header)
-    @jwt_required()
+
     def get(self, user_id):
         student = StudentModel.query.filter_by(id=user_id).first()
         if student:
